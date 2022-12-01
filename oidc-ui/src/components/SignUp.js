0da9ab84d1bd32f249a8fe0552fbec55b67be8d2
Anshul <Anshulv1401@gmail.com>
2022-12-01 17:56:46 +0530

    update sign up button name

--- oidc-ui/src/components/SignUp.js
+++ oidc-ui/src/components/SignUp.js
@@ -120,7 +120,7 @@ export default function SignUp() {
             type="button"
             className="relative w-full text-black bg-gray-50 shadow-lg hover:bg-gray-100  font-medium rounded-lg text-sm px-5 py-2.5 flex items-center mr-2 mb-2"
           >
-            Sign in with MOSIP
+            Sign up with MOSIP
             <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
               <img className="flex mr-1 ml-1 w-6 h-6" src="mosip_logo.png" />
             </div>
